8006bf79b48c2c292c13853a7c212e1c2789d8c5
Pauline-njuguna <paulinenjuguna.w@gmail.com>
2023-03-27 10:50:49 +0300

    Added the home file for the home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+// import image from  "./image"
 
 function Home() {
   return (
-    <div className="home">
-      {/* <div className="image-section">
-        <img src={image} alt="Pet" />
-      </div> */}
-      <div className="text-section">
-        <p>
-          VETERINARY CLINIC IN NAIROBI <br></br>
-          The leading vet clinic , top clinic .
-          modern specialist.
-        </p>
+    <div class="home">
+      <div className="image-section">
+        <img
+          src="src/components/image/alvan-nee-ZCHj_2lJP00-unsplash.jpg"
+          alt="Pet"
+        />
       </div>
+      <h1>Welcome to Our Veterinary Service App</h1>
+      <p>
+        We offer a wide range of veterinary services to help your furry friends
+        stay healthy and happy. <br></br>
+        Our team of experienced vets and staff are here to provide the best care
+        possible for your pets.
+      </p>
+
+      {/* <h2>Our Services</h2>
+      <ul>
+        <li>Preventive Care</li>
+        <li>Dental Care</li>
+        <li>Surgery</li>
+        <li>Emergency Services</li>
+      </ul> */}
     </div>
   );
 }
