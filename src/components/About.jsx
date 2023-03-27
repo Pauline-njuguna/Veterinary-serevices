6302522781f4f4229e3306065c3a737b8a3dc735
Pauline-njuguna <paulinenjuguna.w@gmail.com>
2023-03-27 10:50:31 +0300

    Added the about section

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Our Veterinary Service</h1>
+      <h2>Our Mission</h2>
+      <p>
+        Our mission is to provide the highest quality veterinary care to pets
+        and their owners in a compassionate and supportive environment. We
+        strive to offer personalized care that meets the individual needs of
+        each pet and their family.
+      </p>
+      <h2>Our Vision</h2>
+      <p>
+        Our vision is to be a trusted partner in the health and well-being of
+        pets in our community. We aim to be a leading veterinary service that
+        provides exceptional care, education, and support to pets and their
+        families.
+      </p>
+    </div>
+  );
+}
+
+export default About;
