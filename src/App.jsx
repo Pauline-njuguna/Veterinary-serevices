f8c367e6e3ceb51bfc523b698ae787171bc7189f
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-03-29 17:42:10 +0300

    Petform

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./components/index.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Service";
+import Petform from "./components/PetForm";
 
 function App() {
   return (
