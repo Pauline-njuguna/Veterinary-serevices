ffd368e2333f953ac6020a7735dbaf5f94c3d75e
Pauline-njuguna <paulinenjuguna.w@gmail.com>
2023-03-30 12:05:01 +0300

    imported the sign up and the login paths

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import "./components/index.css";
 import Home from "./components/Home";
 import Petform from "./components/PetForm";
 import AppointmentForm from "./components/AppointmentForm";
-// import SignUp from "./components/Signup"
-// import LogIn from "./components/Login"
+import SignUp from "./components/Signup/Signup"
+import LogIn from "./components/Signup/Login"
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route exact path="/signup-login" element={<SignUp />} /> */}
-        {/* <Route exact path="/login" element={<LogIn />} /> */}
+        <Route exact path="/signup-login" element={<SignUp />} />
+        <Route exact path="/login" element={<LogIn />} />
         <Route exact path="/appointments" element={<AppointmentForm />} />
         <Route exact path="/petform" element={<Petform />} />
       </Routes>
